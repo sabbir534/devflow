3f35edb89b87dce2b1b81eebeac3fefa936f8ddc
Sabbir Ahmed <prince534534@gmail.com>
2025-08-08 21:16:10 +0600

    Setup Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to NXTJS</h1>
+      <h1 className="h1-bold">Welcome to NXTJS</h1>
+      <h1 className="h1-bold font-space-grotesk">Everyone</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
